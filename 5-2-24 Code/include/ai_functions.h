90399fd9f4fe32b6b35bf18a5d0a27606791ef9b
Randy <randylujano@gmail.com>
2024-05-02 19:15:59 -0700

    Update Title
    Update

---
+++ 5-2-24 Code/include/ai_functions.h
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Copyright (c) Innovation First 2023 All rights reserved.                */
+/*    Licensed under the MIT license.                                         */
+/*                                                                            */
+/*    Module:     ai_functions.cpp                                            */
+/*    Author:     VEX Robotics Inc.                                           */
+/*    Created:    11 August 2023                                              */
+/*    Description:  Header for AI robot movement functions                    */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include <vex.h>
+#include <robot-config.h>
+
+// Calculates the distance to a given target (x, y)
+float distanceTo(double target_x, double target_y,vex::distanceUnits unit);
+
+// Moves the robot to a specified position and orientation
+void moveToPosition(double target_x, double target_y, double target_theta, int Dspeed, int Tspeed, bool frontfacing);
+
+// Finds a target object based on the specified type
+DETECTION_OBJECT findTarget(int type);
+
+// Retrieves an object (e.g. from the ground or a dispenser)
+void getObject(int type);
+
+// Turns the robot to a specific angle with given tolerance and speed
+void turnTo(double angle, int speed, bool frontfacing);
---
+++ 5-2-24 Code/include/robot-config.h
@@ -0,0 +1,12 @@
+using namespace vex;
+
+extern brain Brain;
+
+
+extern controller Controller;
+extern Drive Chassis;
+extern gps GPS;
+extern motor_group Intake ; 
+extern optical Balldetect;
+extern Field field;
+
